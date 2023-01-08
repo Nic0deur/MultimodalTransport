ae6b9fa3662808664028ae79c0cf0ee0170d5677
Nic0deur <119055275+Nic0deur@users.noreply.github.com>
2023-01-08 15:51:34 +0100

    Update Metro.cpp

--- MoyenTransport/Metro.cpp
+++ MoyenTransport/Metro.cpp
@@ -1,1 +1,19 @@
 #include "Metro.h"
+#include <vector>
+using namespace std;
+
+Metro::Metro(string nom, int max, double vitesse) {
+  setNom(nom);
+  setMax(max);
+  setVitesse(vitesse);
+}
+void Metro::setNom(string nom){
+   this->nom = nom;
+}
+void Metro::setMax(int max){
+  this->max=max:
+}
+void Metro::setVitesse(double vitesse){
+  this->vitesse=vitesse
+}  
+  
